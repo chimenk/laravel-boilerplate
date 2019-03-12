f03176f113e5e40bd9daa5bf7fddaee0867d36a2
Yandi <yandikurniawan@gmail.com>
2019-03-13 00:43:26 +0700

    Fixing sidebar menu

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -7,7 +7,9 @@
       <a href="index.html">St</a>
     </div>
     <ul class="sidebar-menu">
-      <li><a href="{{ route('home') }}" class="nav-link"><i class="far fa-square"></i> <span>Dashboard</span></a></li>
+      <li><a href="{{ route('home') }}" class="nav-link"><i class="far fa-square"></i> &nbsp;<span>Dashboard</span></a></li>
+      <li class="menu-header">Administration</li>
+      <li><a class="nav-link" href="/logs"><i class="fas fa-fire"></i> &nbsp;<span>Logs</span></a></li>
     </ul>
     <div class="p-3 mt-4 mb-4 hide-sidebar-mini">
       <a href="documentation.html" class="btn btn-primary btn-lg btn-icon-split btn-block">
